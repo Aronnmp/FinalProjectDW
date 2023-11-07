2fa086d71e11e7995836451efd872cac1b9d0b18
DiegoRJ24 <diegorodriguezjaime11@gmail.com>
2023-11-07 12:14:05 -0600

    Proyecto Final Desarrollo Web

---
+++ OneDrive/Documentos/NetBeansProjects/proyecto1.0/src/main/java/com/proyecto/service/FirebaseStorageService.java
@@ -0,0 +1,24 @@
+
+package com.proyecto.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com#
+    final String BucketName = "techshop-d6b25.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-d6b25-firebase-adminsdk-mshy3-0e13e9beb7.json";
+}
+
